bff23727e9c57eb1cc003249bb2227cbdc0b2a78
Matt smith <guitarbass95@gmail.com>
2022-10-28 00:19:38 -0400

    working on config class and thinking of how to handle devices. Most of main will need to be refactored

--- ht_flask/mongo_service.py
+++ ht_flask/mongo_service.py
@@ -90,3 +90,60 @@ class dbm:
             devs["devices"].append(dev_dict)
         #print(devs)
         return devs
+
+#class to standardize the config access
+class configuration:
+    MIN_REFRESH_RATE = 0
+    MAX_REFRESH_RATE = 1000
+
+    def __init__(self, db):
+        _db = db
+        _config_collection = db['config']
+        self.load_config()
+
+    def __getattr__(self, name):
+        if name in self.config.keys():
+            return self.config[str(name)]
+        else:
+            return None
+
+    def __setattr__(self, __name: str, __value) -> None:
+        if __name in self.config.keys():
+            if __name == "m_sync_refresh_rate":
+                if __value < self.MIN_REFRESH_RATE or __value > self.MAX_REFRESH_RATE:
+                    return
+            self.config[__name] = __value
+
+    def __repr__(self) -> str:
+        return json.dumps(self.config, indent=4)
+
+    def set_and_push(self, key, value):
+        self.config[key] = value
+        self.push_config()
+
+    #load config into class
+    #checks existence, loads default if 
+    #not found
+    def load_config(self):
+        if len(list(self._config_collection.find({}))) == 0:
+            try:
+                with open(default_config_path, 'r') as cf_file:
+                    #todo: add try/except 
+                    self.config = json.load(cf_file)
+                    self.push_config()
+            except(FileNotFoundError):
+                print("ERROR:default config file not found and no config exists within db")
+        else:
+            self.config = self._config_collection.find_one({}, {"_id":0})["ht_server_config"]
+
+    #push config back to db
+    def push_config(self):
+        self._config_collection.update_one({}, {"$set":{"ht_server_config":self.config}})
+
+    def save_config(self, filepath=default_config_path):
+        conf = self._config_collection.find_one({}, {"_id":0})
+        if conf != None:
+            cur_config = conf
+            with open(filepath, 'w+') as cf_file:
+                json.dump(cur_config, cf_file, indent=4)
+
